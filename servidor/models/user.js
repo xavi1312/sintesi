ee994fc23c67905d7ac86bbee2e6a21f3a98e86d
Xavi <xavierisachalemany@gmail.com>
2019-04-22 15:47:54 +0200

    jeneracio del token

--- servidor/models/user.js
+++ servidor/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const uniqueValidator = require('mongoose-unique-validator');
 const crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
 const userSchema = Schema({
     name: {
@@ -27,6 +28,19 @@ userSchema.methods.setPassword = (password) => {
 userSchema.methods.validPassword = (password) => {
     var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
     return this.hash === hash;
+};
+
+userSchema.methods.generateJWT = () => {
+    var expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7);
+
+    var jwt = jwt.sign({
+        _id: this._id,
+        email: this.email,
+        name: this.name,
+        exp: parseInt(expiry.getTime() / 1000),
+      }, "Projecte_FINAL");
+    return jwt;
 };
 
 userSchema.plugin(uniqueValidator);
