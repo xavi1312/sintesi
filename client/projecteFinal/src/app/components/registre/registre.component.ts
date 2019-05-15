f300dcf42b5cf5be754f0b5954f74d9deb32f725
Xavi <xavidaw@gmail.com>
2019-05-15 22:44:06 +0200

    construccio registre

--- client/projecteFinal/src/app/components/registre/registre.component.ts
+++ client/projecteFinal/src/app/components/registre/registre.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-registre',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registre.component.scss']
 })
 export class RegistreComponent implements OnInit {
+  formRegistre: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.formRegistre = this.formBuilder.group({
+      email: [Validators.required, Validators.email],
+      contrasenya: [Validators.required],
+      contrasenya2: [Validators.required],
+      politica: [Validators.required]
+    }, {
+        validator: this.comparePass('password', 'confirmPassword')
+    });
   }
 
+  comparePass(pass1, pass2){
+    return (pass1 === pass2) ? true : false
+  }
 }
